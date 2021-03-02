cc34a20c1f689c00543eceb928e7f0e5c192c707
Nordmann Arne (CR/ADT3) <arne.nordmann@de.bosch.com>
2021-03-02 10:04:44 +0100

    Modified example updaters to work with IDs
    Signed-off-by: Nordmann Arne (CR/ADT3) <arne.nordmann@de.bosch.com>

--- micro_ros_diagnostic_updater/example/example_processor_updater.c
+++ micro_ros_diagnostic_updater/example/example_processor_updater.c
@@ -22,16 +22,40 @@
 
 static int my_diagnostic_temp = 0;
 
-const char * my_diagnostic_function()
+rcl_ret_t
+my_diagnostic_temperature(diagnostic_value_t * temp_kv)
 {
-  static char dvalue[20] = "xx degrees";
+  // Fake a temperature
   ++my_diagnostic_temp;
   if (my_diagnostic_temp > 99) {
     my_diagnostic_temp -= 0;
   }
-  snprintf(dvalue, sizeof(dvalue), "%d degrees", my_diagnostic_temp);
+  rclc_diagnostic_value_set_int(temp_kv, my_diagnostic_temp);
 
-  return dvalue;
+  // Calculate the diagnostic level
+  if (my_diagnostic_temp > 85) {
+    rclc_diagnostic_value_set_level(temp_kv, 2);
+  } else if (my_diagnostic_temp > 75) {
+    rclc_diagnostic_value_set_level(temp_kv, 1);
+  } else {
+    rclc_diagnostic_value_set_level(temp_kv, 0);
+  }
+  return RCL_RET_OK;
+}
+
+rcl_ret_t
+my_diagnostic_load(diagnostic_value_t * load_kv)
+{
+  // Fake a processor load
+  rclc_diagnostic_value_set_int(load_kv, my_diagnostic_temp / 2);
+
+  // Calculate the diagnostic level
+  if (my_diagnostic_temp > 46) {
+    rclc_diagnostic_value_set_level(load_kv, 1);
+  } else {
+    rclc_diagnostic_value_set_level(load_kv, 0);
+  }
+  return RCL_RET_OK;
 }
 
 int main(int argc, const char * argv[])
@@ -71,47 +95,59 @@ int main(int argc, const char * argv[])
 
   // updater
   diagnostic_updater_t updater;
-  rc = rclc_diagnostic_updater_init(
-    &updater,
-    &my_node,
-    "my_updater",
-    "mocked hardware monitoring",
-    "hw-id-42");
+  rc = rclc_diagnostic_updater_init(&updater, &my_node, 17, 42);
   if (rc != RCL_RET_OK) {
     printf("Error in creating diagnostic updater\n");
     return -1;
   }
-
   diagnostic_task_t task;
   rc = rclc_diagnostic_task_init(
-    &task,
-    "my_temperatur",
-    &my_diagnostic_function);
+    &task, 0,
+    &my_diagnostic_temperature);
   if (rc != RCL_RET_OK) {
     printf("Error in creating diagnostic task\n");
     return -1;
   }
+  diagnostic_task_t ltask;
+  rc = rclc_diagnostic_task_init(
+    &ltask, 1,
+    &my_diagnostic_load);
+  if (rc != RCL_RET_OK) {
+    printf("Error in creating diagnostic website checker\n");
+    return -1;
+  }
 
+  // adding tasks
   rc = rclc_diagnostic_updater_add_task(
     &updater,
     &task);
   if (rc != RCL_RET_OK) {
-    printf("Error in adding diagnostic task\n");
+    printf("Error in adding diagnostic temp task\n");
+    return -1;
+  }
+  rc = rclc_diagnostic_updater_add_task(
+    &updater,
+    &ltask);
+  if (rc != RCL_RET_OK) {
+    printf("Error in adding diagnostic website task\n");
     return -1;
   }
 
-  for (unsigned int i = 0; i < 2; ++i) {
+  for (unsigned int i = 0; i < 100; ++i) {
+    printf("Publishing processor diagnostics\n");
     rc = rclc_diagnostic_updater_update(&updater);
     if (rc != RCL_RET_OK) {
-      printf("Error in publishing diagnostics\n");
+      printf("Error in publishing temp diagnostics\n");
       return -1;
     }
     sleep(1);
   }
 
+  rclc_diagnostic_updater_fini(&updater, &my_node);
   if (rc != RCL_RET_OK) {
     printf("Error while cleaning up!\n");
     return -1;
   }
+
   return 0;
 }
---
+++ micro_ros_diagnostic_updater/example/example_website_checker.c
@@ -0,0 +1,126 @@
+// Copyright (c) 2020 - for information on the respective copyright owner
+// see the NOTICE file and/or the repository
+// https://github.com/micro-ROS/micro_ros_diagnostics.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#include <stdio.h>
+#include <unistd.h>
+
+#include <rclc/executor.h>
+
+#include <micro_ros_diagnostic_updater/micro_ros_diagnostic_updater.h>
+#include <micro_ros_diagnostic_msgs/msg/micro_ros_diagnostic_status.h>
+
+static int my_diagnostic_status = 0;
+static int my_website_status = 0;
+
+rcl_ret_t
+my_diagnostic_website_check(diagnostic_value_t * kv)
+{
+  ++my_diagnostic_status;
+  if (my_diagnostic_status > 99) {
+    my_diagnostic_status -= 0;
+  }
+  if (my_diagnostic_status % 13 == 0) {
+    my_website_status = 404;
+    rclc_diagnostic_value_set_level(kv, 1);
+  } else if (my_diagnostic_status % 17 == 0) {
+    my_website_status = 500;
+    rclc_diagnostic_value_set_level(kv, 2);
+  } else {
+    my_website_status = 200;
+    rclc_diagnostic_value_set_level(kv, micro_ros_diagnostic_msgs__msg__MicroROSDiagnosticStatus__OK);
+  }
+  rclc_diagnostic_value_lookup(kv, my_website_status);
+
+  return RCL_RET_OK;
+}
+
+int main(int argc, const char * argv[])
+{
+  rcl_context_t context = rcl_get_zero_initialized_context();
+  rcl_init_options_t init_options = rcl_get_zero_initialized_init_options();
+  rcl_allocator_t allocator = rcl_get_default_allocator();
+  rcl_ret_t rc;
+
+  // create init_options
+  rc = rcl_init_options_init(&init_options, allocator);
+  if (rc != RCL_RET_OK) {
+    printf("Error rcl_init_options_init.\n");
+    return -1;
+  }
+
+  // create context
+  rc = rcl_init(argc, argv, &init_options, &context);
+  if (rc != RCL_RET_OK) {
+    printf("Error in rcl_init.\n");
+    return -1;
+  }
+
+  // create rcl_node
+  rcl_node_t my_node = rcl_get_zero_initialized_node();
+  rcl_node_options_t node_ops = rcl_node_get_default_options();
+  rc = rcl_node_init(
+    &my_node,
+    "node_0",
+    "",
+    &context,
+    &node_ops);
+  if (rc != RCL_RET_OK) {
+    printf("Error in rcl_node_init\n");
+    return -1;
+  }
+
+  // updater
+  diagnostic_updater_t updater;
+  rc = rclc_diagnostic_updater_init(&updater, &my_node, 00, 42);
+  if (rc != RCL_RET_OK) {
+    printf("Error in creating diagnostic updater\n");
+    return -1;
+  }
+  diagnostic_task_t task;
+  rc = rclc_diagnostic_task_init(
+    &task, 23,
+    &my_diagnostic_website_check);
+  if (rc != RCL_RET_OK) {
+    printf("Error in creating diagnostic task\n");
+    return -1;
+  }
+
+  // adding tasks
+  rc = rclc_diagnostic_updater_add_task(
+    &updater,
+    &task);
+  if (rc != RCL_RET_OK) {
+    printf("Error in adding diagnostic temp task\n");
+    return -1;
+  }
+
+  for (unsigned int i = 0; i < 100; ++i) {
+    printf("Publishing website diagnostics\n");
+    rc = rclc_diagnostic_updater_update(&updater);
+    if (rc != RCL_RET_OK) {
+      printf("Error in publishing website diagnostics\n");
+      return -1;
+    }
+    sleep(1);
+  }
+
+  rclc_diagnostic_updater_fini(&updater, &my_node);
+  if (rc != RCL_RET_OK) {
+    printf("Error while cleaning up!\n");
+    return -1;
+  }
+
+  return 0;
+}
